b2a6c414816bde7bcc87cb96a86a4c86c9939e18
neerajmiles <neeraj.yadav@miles.in>
2014-09-09 21:41:43 +0530

    First Commit

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,23 @@
+import com.miles.auth.Role
+import com.miles.auth.User
+import com.miles.auth.UserRole
+
+class BootStrap {
+
+    def init = { servletContext ->
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def adminUser = new User(username: 'admin', password: 'password')
+        adminUser.save(flush: true)
+        def testUser = new User(username: 'user', password: 'password')
+        testUser.save(flush: true)
+
+        UserRole.create testUser, userRole, true
+
+        UserRole.create adminUser, adminRole, true
+
+        }
+    def destroy = {
+    }
+}
